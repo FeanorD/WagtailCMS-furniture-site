70d34a68d8b9d8034541ebc02525f95cf482751e
root <root@Dmitry.localdomain>
2019-11-28 14:45:53 +0200

    fix db problems

--- mekasite/settings/dev.py
+++ mekasite/settings/dev.py
@@ -12,6 +12,16 @@ ALLOWED_HOSTS = ['*']
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['POSTGRES_DB'],
+        'USER': os.environ['POSTGRES_USER'],
+        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 try:
